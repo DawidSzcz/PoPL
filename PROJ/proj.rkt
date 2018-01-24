fbabc7a593ab895597c47292917951e20328ea4b
Dawid Szczyrk <dawidszczyrj@gmail.com>
2018-01-24 12:06:37 +0100

    sss

--- PROJ/proj.rkt
+++ PROJ/proj.rkt
@@ -29,11 +29,14 @@
                    ;(print-predicates)
                    (cases term term-query
                      (a-term (id args) 
-                             (let ((env (make-init-env args)))
-                               (process term-query env)
-                               ;(print-unif)
-                               ;(display env)
-                               (resolve-query env)))
+                             (letrec ((env (make-init-env args))
+                                       (backtrack (lambda (clause)
+                                                    (call (cadr clause) (caddr clause) (car clause))
+                                                    (display (resolve-query env)) (newline)
+                                                    (backtrack (call-next)))))
+                                      (call-predicate id args env)
+                                      (backtrack (call-next)))
+                             )
                      (else (eopl:error 'invalid-query term-query))
                      )))))
   
@@ -47,7 +50,7 @@
              (env (process-clauses clauses new-env env)))
         (cases retype env
           (fail (string) 
-                  ;(print-cs) 
+                ;(print-cs) 
                 (let ((new-clause (call-next)))
                   ;(display new-clause) (newline)
                   ;(print-unif) 
@@ -69,7 +72,8 @@
     (lambda (t env)
       (cases term t
         (a-term (id args)
-                (call env args (call-predicate id args env)))
+                (call-predicate id args env)
+                (call (call-next)))
         (a-variable (var) env)
         (a-unification (t1 t2) (unify (get-var t1) t2))
         (a-assignment (t1 exp) (let* ((e (value-of exp env))
@@ -77,13 +81,14 @@
         (a-literal (num) env))))
 
 
-(process-program(scan&parse "pow(5, $z) ? pow(1, 1) :- . pow($y, $z) :- is($a, -($y,1)); pow($a, $r); is($z, *($y, $r))."  ))
-(process-program(scan&parse "test($z) ? test($z) :- is($z, 4); is($z, 3). test(4) :-."  ))
-(process-program(scan&parse "pow(1, $z) ? pow(1, 1) :- . pow($y, $z) :- is($a, -($y,1)); pow($a, $r); is($z, *($y, $r))."  ))
+  (process-program(scan&parse "pow(10, $z) ? pow(1, 1) :- . pow($y, $z) :- is($a, -($y,1)); pow($a, $r); is($z, *($y, $r)). pow($a, $b) :- is($b, $a)."  ))
+  (process-program(scan&parse "pow(10, $z) ? pow(1, 1) :- . pow($y, $z) :- is($a, -($y,1)); pow($a, $r); is($z, *($y, $r))."  ))
+  (process-program(scan&parse "test($z) ? test($z) :- is($z, 4); is($z, 3). test(4) :-."  ))
+  (process-program(scan&parse "pow(1, $z) ? pow(1, 1) :- . pow($y, $z) :- is($a, -($y,1)); pow($a, $r); is($z, *($y, $r))."  ))
 
-(process-program(scan&parse "test(3, $z, $y) ? test($z, $y, 1) :- is($y, $z)."  ))
-(process-program(scan&parse "test(3, $z, $y, $k) ? test($z, $y, 1, $r) :- is($y, $z); t($r). t(5) :-."  ))
-(process-program(scan&parse "test(3, $z, $y, $k) ? test($z, $y, 1, $r) :- is($y, $z); t($a); is($r, $a). t(5) :-."  ))
+  (process-program(scan&parse "test(3, $z, $y) ? test($z, $y, 1) :- is($y, $z)."  ))
+  (process-program(scan&parse "test(3, $z, $y, $k) ? test($z, $y, 1, $r) :- is($y, $z); t($r). t(5) :-."  ))
+  (process-program(scan&parse "test(3, $z, $y, $k) ? test($z, $y, 1, $r) :- is($y, $z); t($a); is($r, $a). t(5) :-."  ))
 
-)
+  )
 
